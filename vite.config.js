518dc58b12001e14224fcfb46e2501d0cd19d0c7
Sudhir Kumar Yadav <145853393+YKSudhir@users.noreply.github.com>
2024-10-20 17:53:40 +0530

    vite.config file modified

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     },
   },
   server: {
-    host: true, // This makes the server accessible externally
-    port: parseInt(import.meta.env.VITE_PORT) || 4173, // Use VITE_PORT from env file or default to 4173
+    host: "0.0.0.0", // Bind to 0.0.0.0 to make it accessible externally
+    port: parseInt(import.meta.env.VITE_PORT) || 4173, // Use the correct port
   },
 });
