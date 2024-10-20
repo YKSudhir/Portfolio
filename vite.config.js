4224e3584745fce2dd642d13843b02d3554806d3
Sudhir Kumar Yadav <145853393+YKSudhir@users.noreply.github.com>
2024-10-20 17:49:08 +0530

    vite.config file modified

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  css:{
-    modules:{
-      localsConvention:"camelCase"
-    }
-  }
-})
+  css: {
+    modules: {
+      localsConvention: "camelCase",
+    },
+  },
+  server: {
+    host: true, // This makes the server accessible externally
+    port: parseInt(import.meta.env.VITE_PORT) || 4173, // Use VITE_PORT from env file or default to 4173
+  },
+});
